5f26ef29bb89d1e6cec183d4d985f01582a96b0f
Pablo de la Peña <p@shedcollective.org>
2015-11-23 11:05:16 +0000

    Removing `$_caller`, pointless really.

--- src/Model/Testimonial.php
+++ src/Model/Testimonial.php
@@ -31,11 +31,10 @@ class Testimonial extends Base
     /**
      * This method applies the conditionals which are common across the get_*()
      * methods and the count() method.
-     * @param array  $data    Data passed from the calling method
-     * @param string $_caller The name of the calling method
+     * @param  array $data Data passed from the calling method
      * @return void
      **/
-    protected function _getcount_common($data = array(), $_caller = null)
+    protected function _getcount_common($data = array())
     {
         if (!empty($data['keywords'])) {
 
@@ -54,6 +53,6 @@ class Testimonial extends Base
             );
         }
 
-        parent::_getcount_common($data, $_caller);
+        parent::_getcount_common($data);
     }
 }
