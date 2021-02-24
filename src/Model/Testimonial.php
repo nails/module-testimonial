52cf857fd1b440091995572414d5cb980d1dc79f
Pablo de la Peña <p@shedcollective.org>
2021-02-24 10:57:58 +0000

    Use constants for model configuration

--- src/Model/Testimonial.php
+++ src/Model/Testimonial.php
@@ -16,14 +16,17 @@ use Nails\Common\Model\Base;
 
 class Testimonial extends Base
 {
+    const TABLE               = NAILS_DB_PREFIX . 'testimonial';
+    const DEFAULT_SORT_COLUMN = 'quote';
+
+    // --------------------------------------------------------------------------
+
     /**
      * Model constructor
      **/
     public function __construct()
     {
         parent::__construct();
-        $this->table             = NAILS_DB_PREFIX . 'testimonial';
-        $this->defaultSortColumn = 'quote';
         $this->searchableFields  = ['quote', 'quote_by'];
     }
 }
